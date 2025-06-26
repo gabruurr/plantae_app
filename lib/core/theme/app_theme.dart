2640f8f5363da0f9b9074d77b4b018a2af2ca694
unknown <gabryelaraujo2019@gmail.com>
2025-06-26 04:03:42 -0300

    feat: criando tema da aplicacao

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primaryColor = Color(0xFF34A853);
+  static const Color accentColor = Color(0xFF5EBF7F);
+  static const Color backgroundColor = Color(0xFFF5F7F5);
+  static const Color surfaceColor = Color(0xFFFFFFFF);
+  static const Color textColor = Color(0xFF202124);
+  static const Color secondaryTextColor = Color(0xFF5F6368);
+
+}
