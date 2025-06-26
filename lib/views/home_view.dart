9a1fc8097eae8b59de5d71189da7e69284c433a9
unknown <gabryelaraujo2019@gmail.com>
2025-06-26 04:45:17 -0300

    feat: criando barra de pesquisa

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -10,10 +10,34 @@ class HomeView extends StatelessWidget {
     final viewModel = Provider.of<PlantViewModel>(context, listen: false);
 
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Plantae'),
-          centerTitle: false,
-        ),
-        body: Placeholder());
-  }
-}
+      appBar: AppBar(
+        title: const Text('Plantae'),
+        centerTitle: false,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0, bottom: 16.0),
+              child: TextField(
+                onChanged: (value) {
+                  viewModel.searchPlants(value);
+                },
+                decoration: InputDecoration(
+                  hintText: 'Pesquisar por nome ou espécie...',
+                  prefixIcon: const Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey.shade200,
+                ),
+              ),
+            ),
+          ],),
+      ),
+    );
+        
+}}
