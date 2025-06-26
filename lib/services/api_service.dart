3a0c88f510e2d265f3a21737ce1607b63599cb5f
unknown <gabryelaraujo2019@gmail.com>
2025-06-26 04:05:55 -0300

    chore: estabelecendo conexao com api criada do supabase

---
+++ lib/services/api_service.dart
@@ -0,0 +1,6 @@
+class ApiService {
+  static const String _baseUrl =
+      'https://enfuptvhruhrmcnqggzv.supabase.co/rest/v1';
+  static const String _apiKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVuZnVwdHZocnVocm1jbnFnZ3p2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4OTQ1MzMsImV4cCI6MjA2NjQ3MDUzM30.9x9KP5etthTv-EfsXbVw2x4rUg73hLOQZ1MTeiL94gM';
+}
