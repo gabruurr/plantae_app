ff8d7d59ba019bbf4a65fa8e3b889ab48a3f6052
unknown <gabryelaraujo2019@gmail.com>
2025-06-26 05:04:19 -0300

    style: finalizando theme

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   static const Color primaryColor = Color(0xFF34A853);
@@ -8,4 +9,85 @@ class AppTheme {
   static const Color textColor = Color(0xFF202124);
   static const Color secondaryTextColor = Color(0xFF5F6368);
 
+  static ThemeData get lightTheme {
+    return ThemeData(
+      useMaterial3: true,
+      scaffoldBackgroundColor: backgroundColor,
+      colorScheme: const ColorScheme.light(
+        primary: primaryColor, //cor do texto
+        secondary: accentColor,
+        surface: surfaceColor,
+        error: Colors.red,
+        onPrimary: Colors.white,
+        onSecondary: Colors.black,
+        onSurface: textColor,
+        onError: Colors.white,
+      ),
+      textTheme:
+          GoogleFonts.poppinsTextTheme(ThemeData.light().textTheme).copyWith(
+        displaySmall:
+            const TextStyle(fontWeight: FontWeight.bold, color: textColor),
+        headlineMedium:
+            const TextStyle(fontWeight: FontWeight.bold, color: textColor),
+        titleLarge:
+            const TextStyle(fontWeight: FontWeight.bold, color: textColor),
+        bodyLarge: const TextStyle(color: textColor),
+        bodyMedium: const TextStyle(color: secondaryTextColor),
+      ),
+      appBarTheme: AppBarTheme(
+        backgroundColor: backgroundColor,
+        foregroundColor: textColor,
+        elevation: 0,
+        centerTitle: true,
+        titleTextStyle: GoogleFonts.poppins(
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+          color: textColor,
+        ),
+      ),
+      cardTheme: CardTheme(
+        color: surfaceColor,
+        elevation: 0.5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.0),
+          borderSide: const BorderSide(color: Colors.transparent),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.0),
+          borderSide: BorderSide(color: Colors.grey.shade300),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.0),
+          borderSide: const BorderSide(color: primaryColor, width: 2.0),
+        ),
+        filled: true,
+        fillColor: Colors.white,
+        labelStyle: const TextStyle(color: secondaryTextColor),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color.fromARGB(255, 27, 128, 243),
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+          textStyle: GoogleFonts.poppins(
+            fontWeight: FontWeight.w600,
+            fontSize: 16,
+          ),
+        ),
+      ),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: primaryColor,
+        foregroundColor: Colors.white,
+        shape: CircleBorder(),
+      ),
+    );
+  }
 }
