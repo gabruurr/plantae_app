2bf40f28e12d3dcde093f594403c05c911582419
unknown <gabryelaraujo2019@gmail.com>
2025-06-26 04:17:02 -0300

    feat: finalziada configuracoes da api de carregamento de imagem

--- lib/services/image_service.dart
+++ lib/services/image_service.dart
@@ -1,3 +1,34 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
 class ImageService {
   static const String _apiKey = '6df95ef9f0c14cee435aa746d8fd2b92';
+
+  Future<String?> uploadImage(File imageFile) async {
+    final uri = Uri.parse('https://api.imgbb.com/1/upload?key=$_apiKey');
+    
+    final imageBytes = await imageFile.readAsBytes();
+    final base64Image = base64Encode(imageBytes);
+
+    try {
+      final response = await http.post(
+        uri,
+        body: {
+          'image': base64Image,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final responseData = json.decode(response.body);
+        return responseData['data']['display_url'];
+      } else {
+        print('Erro no upload da imagem: ${response.body}');
+        return null;
+      }
+    } catch (e) {
+      print('Exceção no upload da imagem: $e');
+      return null;
+    }
+  }
 }
