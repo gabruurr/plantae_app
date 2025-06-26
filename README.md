00528f4e3f5e7e9b91cdbf57f48793313fdb83d8
unknown <gabryelaraujo2019@gmail.com>
2025-06-26 03:55:15 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# plantae_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/plantae_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.plantae_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
